ac0018da6f94b6d45c871158087bd4b57a56c2af
Yabesh Murgan <151531028+im-yabi@users.noreply.github.com>
2025-05-10 23:53:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+document.body.innerHTML = "10"
+setTimeout(() => {
+document.body.innerHTML = "9";
+setTimeout(() => {
+document.body.innerHTML = "8";
+setTimeout(() => {
+document.body.innerHTML = "7";
+setTimeout(() => {
+document.body.innerHTML = "6";
+setTimeout(() => {
+document.body.innerHTML = "5";
+setTimeout(() => {
+document.body.innerHTML = "4";
+setTimeout(() => {
+document.body.innerHTML = "3";
+setTimeout(() => {
+document.body.innerHTML = "2";
+setTimeout(() => {
+document.body.innerHTML = "1";
+setTimeout(() => {
+document.body.innerHTML = `<h3 style ="text-shadow: 0 0 5px #FFF, 0 0 10px #FFF, 0 0 15px #FFF, 0 0 20px #49ff18, 0 0 30px #49FF18, 0 0 40px #49FF18, 0 0 55px #49FF18, 0 0 75px #49ff18; ">வானில் நீ உயர்ந்தாலும் வாழ்வதற்கு தரைக்கு தான் வரவேண்டும்.!</h3>`;
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000)
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
